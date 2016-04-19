58b652f6f439013a3b1c88d19258fa87ecc7dab6
Roman <rboyarov91@gmail.com>
2016-04-19 16:29:47 -0400

    set cycling as a setting on new setings page

--- background.js
+++ background.js
@@ -1,11 +1,15 @@
-console.log("running background script");
+isCycle = false;
 chrome.commands.onCommand.addListener( function(command) {
-	console.log('onCommand event received for message: ', command);
+	chrome.storage.sync.get({
+    cycle: true
+  }, function(items) {
+    isCycle = items.cycle;
+  });
+	console.log('value read from storage: ' + isCycle);
     if(command === "switch-tab-left"){
-	    console.log("switching to the right");
 	    chrome.tabs.query({currentWindow: true},
 		    function(tabArray){
-		    	if (tabArray[0].active) {
+		    	if (tabArray[0].active && isCycle) {
 		    		chrome.tabs.update(tabArray[tabArray.length - 1].id, {"active" : true});
 		    		return;
 		    	}
@@ -14,8 +18,6 @@ chrome.commands.onCommand.addListener( function(command) {
 		    	console.log('length: ' + l);
 		    	for (tab in tabArray) {
 		    		index = l - tab ;
-		    		console.log('index: ' + index);
-		    		console.log('checking tab:' + tab + 'is active? ' + tabArray[index].active);
 		    		if (tabArray[index].active) {
 		    			currentFound = true;
 		    		}
@@ -27,21 +29,18 @@ chrome.commands.onCommand.addListener( function(command) {
 		    })
     }
     if(command === "switch-tab-right"){
-	    console.log("switching to the left");
 	    chrome.tabs.query({currentWindow: true,},
 		    function(tabArray){
 		    	currentFound = false;
 		    	for (tab in tabArray) {
-		    		if (tabArray[tabArray.length - 1].active) {
+		    		if (tabArray[tabArray.length - 1].active && isCycle) {
 		    			chrome.tabs.update(tabArray[0].id, {"active" : true});
 		    			return;
 		    		}
-		    		console.log('checking tab:' + tab + 'is active? ' + tabArray[tab].active);
 		    		if (tabArray[tab].active) {
 		    			currentFound = true;
 		    		}
 		    		else if (currentFound) {
-		    			console.log('changing to tab: ' + tab);
 		    			chrome.tabs.update(tabArray[tab].id, {"active" : true});
 		    			break;
 		    		}
